e374727200ff0941a5fecbcb0ca625efa1c241c2
speasley <s@stephenpeasley.com>
2015-07-15 17:45:52 -0600

    Expand tests

--- spec/javascripts/EdittrSpec.js
+++ spec/javascripts/EdittrSpec.js
@@ -1,12 +1,12 @@
 var $table;
 
-beforeEach(function() {
-	loadFixtures("form.html");
-	$table = $("#edit-panel");
-  $table.edittr();
-});
-
 describe("Table", function() {
+
+  beforeEach(function() {
+    loadFixtures("form.html");
+    $table = $("#edit-panel");
+    $table.edittr();
+  });
 
 	it("should exist in the fixture", function() {
 		expect($table).toHaveId("edit-panel");
@@ -16,29 +16,40 @@ describe("Table", function() {
 		expect($table).toHaveClass("edittr");
 	});
   
-	it("should have an editing class when in editing mode", function() {
+	it("should have an 'editing' class when in editing mode", function() {
     $('a.edit').click();
 		expect($table).toHaveClass("editing");
 	});
 		
+	it("should have text content that is converted to an input", function() {
+    expect($("td.name")).toHaveText("Stephen");
+    $('a.edit').click();
+    expect($("td.name input").val()).toBe("Stephen");
+	});
+
+  it("should show the Save and Cancel buttons only when in editing mode", function() {
+    expect($("a.edit")).toBeVisible();
+    $('a.edit').click();
+    expect($("a.edit")).toBeHidden();
+    expect($("a.cancel")).toBeVisible();
+    expect($("a.save")).toBeVisible();
+  });
+
 });
 
 describe("Options", function() {
 
 	describe("I18n", function() {
 	
-		it("should accept an cancelText value", function() {
-			$table.edittr({cancelText:"Fuggedit"});
-		});
-		
-		it("should accept an editText value", function() {
-			$table.edittr({editText:"Edito"});
-		});
-		
-		it("should accept an saveText value", function() {
-			$table.edittr({saveText:"Le save"});
-		});
+    beforeEach(function() {
+      loadFixtures("form.html");
+      $table = $("#edit-panel");
+      $table.edittr({cancelText:"Fuggedit",editText:"Edito",saveText:"Le save"});
+    });
 
+		it("should custom values for cancelText, editText and saveText", function() {
+		});
+		
 	});
 
 	describe("Callbacks", function() {
