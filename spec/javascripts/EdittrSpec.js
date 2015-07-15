6536968fc9257e0b3f295a0642b63140de1b7d92
speasley <s@stephenpeasley.com>
2015-07-15 17:09:50 -0600

    Tighten up tests

--- spec/javascripts/EdittrSpec.js
+++ spec/javascripts/EdittrSpec.js
@@ -1,26 +1,24 @@
-var $element;
+var $table;
 
 beforeEach(function() {
 	loadFixtures("form.html");
-	$element = $("#edit-panel");
+	$table = $("#edit-panel");
+  $table.edittr();
 });
 
 describe("Table", function() {
 
 	it("should exist in the fixture", function() {
-		expect($element).toHaveId("edit-panel");
+		expect($table).toHaveId("edit-panel");
 	});
 	
-	it("should call jquery.edittr()", function() {
-		spyOn($element, 'edittr');
-		$element.edittr();
-		expect($element.edittr).toHaveBeenCalled();
+	it("should have an 'edittr' class", function() {
+		expect($table).toHaveClass("edittr");
 	});
-	
-	it("should have an edittr class", function() {
-		spyOn($element, 'edittr');
-		$element.edittr();
-		expect($element).toHaveClass("edittr");
+  
+	it("should have an editing class when in editing mode", function() {
+    $('a.edit').click();
+		expect($table).toHaveClass("editing");
 	});
 		
 });
@@ -30,15 +28,15 @@ describe("Options", function() {
 	describe("I18n", function() {
 	
 		it("should accept an cancelText value", function() {
-			$element.edittr({cancelText:"Fuggedit"});
+			$table.edittr({cancelText:"Fuggedit"});
 		});
 		
 		it("should accept an editText value", function() {
-			$element.edittr({editText:"Edito"});
+			$table.edittr({editText:"Edito"});
 		});
 		
 		it("should accept an saveText value", function() {
-			$element.edittr({saveText:"Le save"});
+			$table.edittr({saveText:"Le save"});
 		});
 
 	});
@@ -47,7 +45,7 @@ describe("Options", function() {
 
 		it("should execute the callback code", function() {
 			var testVar = "foo"
-			$element.edittr({
+			$table.edittr({
 				onEdit: function() {
 					testVar = "bar"
 				}
